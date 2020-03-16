19583634bb68f0d488ca0997f0c67fb9425269c0
Matheus Pires <mpires@loyall.local>
2020-03-16 18:09:45 -0300

    ...

--- Main.py
+++ Main.py
@@ -2,10 +2,25 @@ from matplotlib import pyplot
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import yfinance as yf
 import datetime
+import talib as ta
+import pandas_datareader.data as web
 
-asset = yf.download('^BVSP','1995-01-27', datetime.date)
-plot_acf(asset['Close'], lags=20)
-pyplot.show()
 
-plot_pacf(asset['Close'], lags=20)
-pyplot.show()
+initDate = datetime.date.today() - datetime.timedelta(days = 252)
+
+endDate = datetime.date.today()
+
+asset = yf.download('BRL=X',start = initDate, end = endDate)
+
+
+
+
+
+
+
+
+# plot_acf(asset['Close'], lags=20)
+# pyplot.show()
+
+# plot_pacf(asset['Close'], lags=20)
+# pyplot.show()
